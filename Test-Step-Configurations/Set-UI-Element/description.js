25f1c03265093cbeb6668f759559f2c36984be84
nidejesus <105457332+nidejesus@users.noreply.github.com>
2024-03-20 16:30:36 -0400

    Update description.js
    Included new Variable

--- Test-Step-Configurations/Set-UI-Element/description.js
+++ Test-Step-Configurations/Set-UI-Element/description.js
@@ -1,26 +1,31 @@
 function generateDescription() {
-    // the global variable 'step' represents the current glide record
-    try {
-        // the global variable 'step' represents the current glide record
+	// the global variable 'step' represents the current glide record
+	try {
+		// the global variable 'step' represents the current glide record
 
-        var tokens = [];
+		var tokens = [];
 
-        var description = "";
+		var description = "";
 
-        // your code here
-        description = "Set the UI element that matches the query selector '{0}' to '{1}'.";
-        if (!gs.nil(step.inputs.u_query_selector.getDisplayValue())) {
-            tokens.push(step.inputs.u_query_selector.getDisplayValue().trim());
-        }
-        if (!gs.nil(step.inputs.u_value.getDisplayValue())) {
-            tokens.push(step.inputs.u_value.getDisplayValue().trim());
-        }
-
-        description = gs.getMessage(description, tokens);
-        return description;
-    } catch (e) {
-        return description + "\n" + e;
-    }
+		// your code here
+		description = "Set the UI element that matches the query selector '{0}' to '{1}'";
+		if (!gs.nil(step.inputs.u_query_selector.getDisplayValue())) {
+			tokens.push(step.inputs.u_query_selector.getDisplayValue().trim());
+		}
+		if (!gs.nil(step.inputs.u_value.getDisplayValue())) {
+			tokens.push(step.inputs.u_value.getDisplayValue().trim());
+		}
+		if (!gs.nil(step.inputs.u_index_of_element.getDisplayValue())) {
+			description = description + "at the index of '{2}'.";
+			tokens.push(step.inputs.u_index_of_element.getDisplayValue().trim());
+		}else{
+			description = description + ".";
+		}
+		description = gs.getMessage(description, tokens);
+		return description;
+	} catch (e) {
+		return description + "\n" + e;
+	}
 
 }
 generateDescription();
