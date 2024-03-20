d3e743c71710e75755cc3a1d189fd61848220efb
nidejesus <105457332+nidejesus@users.noreply.github.com>
2024-03-20 16:33:02 -0400

    Update script.js
    Added the ability to get an element based on the index of the results array.

--- Test-Step-Configurations/Set-UI-Element/script.js
+++ Test-Step-Configurations/Set-UI-Element/script.js
@@ -5,13 +5,13 @@
         var MESSAGE_KEY_NO_VALUE_PROVIDED = "FAILURE: No value provided";
         var MESSAGE_KEY_WAITING_FOR_TIMEOUT = "Waiting {0} seconds before completing the step";
         var MESSAGE_KEY_UI_ELEMENT_NOT_FOUND = "Failure: No UI element matched the selector '{0}'.";
-        var MESSAGE_KEY_NO_CLICK_FUNCTION = "Failure: The UI element that matched the selector '{0}' did not have a 'click' function.";
         var MESSAGE_KEY_SET_VALUE_SUCCESS = "The UI element that matched the selector '{0}' was successfully set.";
         var MESSAGE_KEY_UI_ELEMENT_NOT_VISIBLE = "Failure: UI element matched the selector '{0}' is not visible.";
         var MESSAGE_KEY_UI_ELEMENT_READ_ONLY = "The UI element that matched the selector '{0}' is read only.";
+        var MESSAGE_KEY_INDEX_NOT_A_NUMBER = "FAILURE: The value of the index is not a number";
 
 
-        var messages = new GwtMessage().getMessages([MESSAGE_KEY_NO_QUERY_SELECTOR_PROVIDED, MESSAGE_KEY_NO_VALUE_PROVIDED, MESSAGE_KEY_WAITING_FOR_TIMEOUT, MESSAGE_KEY_UI_ELEMENT_NOT_FOUND, MESSAGE_KEY_NO_VALUE_PROVIDED, MESSAGE_KEY_SET_VALUE_SUCCESS, MESSAGE_KEY_UI_ELEMENT_NOT_VISIBLE, MESSAGE_KEY_UI_ELEMENT_READ_ONLY]);
+        var messages = new GwtMessage().getMessages([MESSAGE_KEY_NO_QUERY_SELECTOR_PROVIDED, MESSAGE_KEY_NO_VALUE_PROVIDED, MESSAGE_KEY_WAITING_FOR_TIMEOUT, MESSAGE_KEY_UI_ELEMENT_NOT_FOUND, MESSAGE_KEY_NO_VALUE_PROVIDED, MESSAGE_KEY_SET_VALUE_SUCCESS, MESSAGE_KEY_UI_ELEMENT_NOT_VISIBLE, MESSAGE_KEY_UI_ELEMENT_READ_ONLY,MESSAGE_KEY_INDEX_NOT_A_NUMBER]);
 
         // Function for updating the message that will appear in the step result.
         function updateStepResultMessage(message) {
@@ -28,20 +28,34 @@
             failStep(messages[MESSAGE_KEY_NO_VALUE_PROVIDED]);
             return;
         }
-
+        // Check the input and fail if the index is NaN
+        if (isNaN(parseInt(step.inputs.u_index_of_element))) {
+            failStep(messages[MESSAGE_KEY_INDEX_NOT_A_NUMBER]);
+            return;
+        }
+        
         // Get inputs
         var querySelector = step.inputs.u_query_selector + "";
         querySelector = querySelector.trim();
 
         var value = step.inputs.u_value + "";
         value = value.trim();
+                
+        var elementIndex = parseInt(step.inputs.u_index_of_element);
 
         try {
             // Gets the window that's loaded into the Test Runner
             var testFrameWindow = g_ui_testing_util.getTestIFrameWindow();
+            var uiElement;
 
-            // Get the uiElement
-            var uiElement = testFrameWindow.jQuery(querySelector);
+            if(elementIndex || elementIndex === 0 ){
+                // Get the uiElement based on selector and the index of resulting search
+                uiElement = testFrameWindow.jQuery(querySelector).eq(elementIndex);
+            }else{
+                // Get the uiElement based only on selector
+                uiElement = testFrameWindow.jQuery(querySelector);
+            }
+            
             //verify element is found, visable, and editable
             if (uiElement.length > 0 && uiElement.is(":visible") && !uiElement.is("[readonly]") && !uiElement.is("[disabled]")) {
                 //Set the elements value and force change event
